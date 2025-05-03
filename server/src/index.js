7a7387b938537733016eeadd7bba1de119c8a8a5
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-03 17:52:20 +0000

    feat(server): Setup server with CORS, cookie-parser, and MongoDB connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,10 @@
+import express from 'express'
 import app from "./app.js";
 import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+import connectDB from "./database/db.js";
 
 dotenv.config({
     path: "./.env"
@@ -7,6 +12,24 @@ dotenv.config({
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
-})
+app.use(
+    cors({
+        origin: process.env.BASE_URL,
+        credentials: true, // to enable storing cookie
+        methods: ["GET", "POST", "DELETE", "OPTIONS"], // not case sensitive
+        allowedHeaders: ["Content-Type", "Authorization"], // case sensitive
+    })
+);
+
+app.use(express.json()); // to accept json data
+app.use(express.urlencoded({ extended: true })); // to accept data from url
+app.use(cookieParser());
+
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+    })
+    .catch((err) => {
+        console.error("Mongodb connection error", err)
+        process.exit(1);
+    })
