1ae4ddee33e3d768771757253985ce64bdc20474
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-04 04:18:21 +0000

    feat(server/app.js): Integrate healthcheck route under /api/v1/users

--- server/src/app.js
+++ server/src/app.js
@@ -2,4 +2,8 @@ import express from 'express'
 
 const app = express()
 
+import healthCheckRoute from './routes/healthCheck.route.js'
+
+app.use("/api/v1/users", healthCheckRoute)
+
 export default app;
