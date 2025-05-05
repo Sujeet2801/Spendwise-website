2e41a9b0341697d307cb322ee80be4542f15310d
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-05 04:59:52 +0000

    chore(server): Add cookie-parser, express.json, urlencoded middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
+
+app.use(
+    cors({
+        origin: process.env.BASE_URL,
+        credentials: true, // to enable storing cookie
+        methods: ["GET", "POST", "DELETE", "OPTIONS"], // not case sensitive
+        allowedHeaders: ["Content-Type", "Authorization"], // case sensitive
+    })
+);
+
+app.use(express.json()); // to accept json data
+app.use(express.urlencoded({ extended: true })); // to accept data from url
+app.use(cookieParser());
 
 import healthCheckRoute from './routes/healthCheck.route.js'
 
