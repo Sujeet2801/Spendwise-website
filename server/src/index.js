b46ad7795517a6562092a1da434ae262f65f7ed6
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2025-05-05 04:57:39 +0000

    chore(server): Move cors, express.json, and urlencoded middleware to app.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,5 @@
-import express from 'express'
 import app from "./app.js";
 import dotenv from 'dotenv'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
 
 import connectDB from "./database/db.js";
 
@@ -11,19 +8,6 @@ dotenv.config({
 })
 
 const PORT = process.env.PORT || 3000
-
-app.use(
-    cors({
-        origin: process.env.BASE_URL,
-        credentials: true, // to enable storing cookie
-        methods: ["GET", "POST", "DELETE", "OPTIONS"], // not case sensitive
-        allowedHeaders: ["Content-Type", "Authorization"], // case sensitive
-    })
-);
-
-app.use(express.json()); // to accept json data
-app.use(express.urlencoded({ extended: true })); // to accept data from url
-app.use(cookieParser());
 
 connectDB()
     .then(() => {
