8c5fb12c80c2d3a3480240bdf7359a9f75cf5f28
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-16 15:55:29 +0530

    refactor(server/routes): Update auth route layout

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
-
-import { changePasswordController, 
-        forgotPasswordRequestController, 
-        getCurrentUserController, 
-        loginUserController, 
-        logoutUserController, 
-        registerUserController, 
-        resendVerificationEmailController, 
-        updateCurrentUserController, 
-        verifyEmailController 
-    } from "../controllers/auth.controller.js";
+import { 
+    changePasswordController, 
+    forgotPasswordRequestController, 
+    getCurrentUserController, 
+    loginUserController, 
+    logoutUserController, 
+    registerUserController, 
+    resendVerificationEmailController, 
+    updateCurrentUserController, 
+    verifyEmailController 
+} from "../controllers/auth.controller.js";
 
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 import { userLoginSchema, userRegistrationSchema } from "../validators/auth.validate.js";
@@ -17,15 +17,40 @@ import { validateUsingZodError } from "../middlewares/validate.middleware.js";
 
 const router = Router();
 
-router.route("/register").post( validateUsingZodError(userRegistrationSchema()), registerUserController);
-router.route("/verify/:token").get(verifyEmailController);
-router.route("/login").post( validateUsingZodError(userLoginSchema()),loginUserController);
-router.route("/logout").get( isAuthenticated, logoutUserController);
-router.route("/me").get(isAuthenticated, getCurrentUserController);
-router.route("/me/update").post(isAuthenticated, updateCurrentUserController);
+router
+    .route("/register")
+    .post( validateUsingZodError(userRegistrationSchema()), registerUserController);
 
-router.route("/resend-email").post(resendVerificationEmailController);
-router.route("/reset-password").post(forgotPasswordRequestController);
-router.route("/reset-password/:token").post(changePasswordController);
+router
+    .route("/verify/:token")
+    .get(verifyEmailController);
+
+router
+    .route("/login")
+    .post( validateUsingZodError(userLoginSchema()),loginUserController);
+
+router
+    .route("/logout")
+    .get( isAuthenticated, logoutUserController);
+
+router
+    .route("/me")
+    .get(isAuthenticated, getCurrentUserController);
+
+router
+    .route("/me/update")
+    .post(isAuthenticated, updateCurrentUserController);
+
+router
+    .route("/resend-email")
+    .post(resendVerificationEmailController);
+
+router
+    .route("/reset-password")
+    .post(forgotPasswordRequestController);
+
+router
+    .route("/reset-password/:token")
+    .post(changePasswordController);
 
 export default router;
