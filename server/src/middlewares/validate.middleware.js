63387d4f3a4a5f868aa4d3b117a70ddd533989b9
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-16 16:25:28 +0530

    refactor(server/middleware): Update layout of validate middleware

--- server/src/middlewares/validate.middleware.js
+++ server/src/middlewares/validate.middleware.js
@@ -2,20 +2,23 @@ import { ApiError } from "../utils/api-error.js";
 import { ZodError } from "zod";
 
 export const validateUsingZodError = (schema) => (req, res, next) => {
+    
     try {
         schema.parse(req.body);
         next();
+
     } catch (err) {
+
         if (err instanceof ZodError) {
         const formattedErrors = err.errors.map(e => ({
             path: e.path.join('.'),
             message: e.message
         }));
-
         
-        throw new ApiError(400, "Validation failed", formattedErrors);
+        return res.status(400).json( new ApiError(400, "Validation failed", formattedErrors));
+
         } else {
-        throw new ApiError(500, "Unexpected error during validation");
+            return res.status(500).json( new ApiError(500, "Unexpected error during validation" ));
         }
     }
 };
