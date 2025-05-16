5249ae3be8763087995cbd0eaffcca94b7d06d14
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-16 16:21:51 +0530

    feat(server/validators): Add Zod schema for expense validation with strict field rules

---
+++ server/src/validators/expense.validate.js
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+const expenseSchema = () => {
+    return z.object({
+        amount: z
+            .number({ invalid_type_error: "Amount must be a number" })
+            .positive("Amount must be greater than 0"),
+
+        category: z
+            .string()
+            .nonempty("Category is required")
+            .refine(
+                (val) =>
+                    [
+                        "Food", "Transport", "Utilities", "Entertainment", "Health",
+                        "Education", "Housing", "Personal Care", "Shopping",
+                        "Travel", "Insurance", "Gifts", "Donations", "Savings",
+                        "Investments", "Taxes", "Fees", "Childcare", "Pets", "Other"
+                    ].includes(val),
+                { message: "Invalid category" }
+            ),
+
+        date: z
+            .string()
+            .refine((val) => !isNaN(Date.parse(val)), {
+                message: "Invalid date format",
+            }),
+
+        notes: z
+            .string()
+            .max(300, "Note should not be greater than 300 char")
+            .optional()
+    });
+};
+
+export { expenseSchema };
